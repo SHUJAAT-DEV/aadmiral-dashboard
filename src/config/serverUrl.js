4ab572b1e71696cd1725d75a0d3a18f5ef5275dc
shujaat <shujaat.dev@taazaa.com>
2023-02-24 23:09:02 +0500

    added new url

--- src/config/serverUrl.js
+++ src/config/serverUrl.js
@@ -1,3 +1,3 @@
 // export const URL = "https://admirals.herokuapp.com";
-// export const URL = "https://admin.aadmirals.com"
-export const URL = "http://aadmiralapi-env.eba-2bmqy7kt.us-east-2.elasticbeanstalk.com"
+export const URL = "https://api.aadmirals.com"
+// export const URL = "http://aadmiralapi-env.eba-2bmqy7kt.us-east-2.elasticbeanstalk.com"
